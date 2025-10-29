e90c552013c151428e70ed10de324cc70456160e
shantunu007 <chakmashantunu.web@gmail.com>
2025-10-29 11:01:46 +0600

    positive numbers only challenge done

--- filter.js
+++ filter.js
@@ -4,3 +4,10 @@ const numbers = [1, 2, 3, 4, 5, 6];
 
 const evenNumbers = numbers.filter(num => num % 2 === 0)
 console.log(evenNumbers);
+
+//Positive Numbers Only
+
+const nums = [-3, 5, -7, 8, 0, 12, -1, 0];
+
+const positiveNumbers = nums.filter(num => num < 0);
+console.log(positiveNumbers);
