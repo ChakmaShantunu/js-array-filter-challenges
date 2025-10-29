bd8419e58191e98e76a7597b576ff4e5ce13433c
shantunu007 <chakmashantunu.web@gmail.com>
2025-10-29 11:15:06 +0600

    filter by starting letter challenge done

--- filter.js
+++ filter.js
@@ -17,3 +17,8 @@ const names = ["Rafi", "Anika", "Tom", "Rahim", "Li"];
 
 const shortNames = names.filter(name => name.length < 4)
 console.log(shortNames);
+
+//Filter by Starting Letter
+const fruits = ["apple", "banana", "cherry", "avocado", "mango"];
+const startingLetter = fruits.filter(fruit => fruit.startsWith("a"))
+console.log(startingLetter);
